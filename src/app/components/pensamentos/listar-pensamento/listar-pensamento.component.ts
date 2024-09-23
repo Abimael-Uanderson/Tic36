4236520fccf0aee3e892eddf27718d8d03a79d17
Abimael Cristovao <abimael.servicos12dt@gmail.com>
2024-09-22 23:56:59 -0300

    first commit

---
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../Pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-listar-pensamento',
+  templateUrl: './listar-pensamento.component.html',
+  styleUrls: ['./listar-pensamento.component.css'],
+})
+export class ListarPensamentoComponent implements OnInit {
+  listaPensamentos: Pensamento[] = [];
+  paginaAtual: number = 1;
+  haMaisPensamentos: boolean = true;
+  filtro: string = '';
+  favoritos: boolean = false;
+  listaFavoritos: Pensamento[] = [];
+  titulo: string = 'Meu Mural';
+
+  constructor(private service: PensamentoService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.service
+      .listar(this.paginaAtual, this.filtro, this.favoritos)
+      .subscribe((listaPensamentos) => {
+        this.listaPensamentos = listaPensamentos;
+      });
+  }
+
+  carregarMaisPensamentos() {
+    this.service
+      .listar(++this.paginaAtual, this.filtro, this.favoritos)
+      .subscribe((listaPensamentos) => {
+        this.listaPensamentos.push(...listaPensamentos);
+        if (!listaPensamentos.length) {
+          this.haMaisPensamentos = false;
+        }
+      });
+  }
+
+  pesquisarPensamentos() {
+    this.haMaisPensamentos = true;
+    this.paginaAtual = 1;
+    this.service
+      .listar(this.paginaAtual, this.filtro, this.favoritos)
+      .subscribe((listaPensamentos) => {
+        this.listaPensamentos = listaPensamentos;
+      });
+  }
+
+  listarFavoritos() {
+    this.titulo = 'Meus Favoritos'
+    this, (this.favoritos = true);
+    this.haMaisPensamentos = true;
+    this.paginaAtual = 1;
+    this.service
+      .listar(this.paginaAtual, this.filtro, this.favoritos)
+      .subscribe((listarPensamentosFavoritos) => {
+        this.listaPensamentos = listarPensamentosFavoritos;
+        this.listaFavoritos = listarPensamentosFavoritos;
+      });
+  }
+
+  recarregarComponente() {
+    this.titulo = 'Meus Mural'
+    this.favoritos = false;
+    this.paginaAtual = 1;
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate([this.router.url]);
+  }
+}
